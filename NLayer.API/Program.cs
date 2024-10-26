80454a388df24f487a65f16df23323e6c9788c5b
deltadijitalajans <57143153+deltadijitalajans@users.noreply.github.com>
2024-10-26 15:45:22 +0300

    a

--- NLayer.API/Program.cs
+++ NLayer.API/Program.cs
@@ -28,10 +28,7 @@ builder.Services.Configure<ApiBehaviorOptions>(options =>
     options.SuppressModelStateInvalidFilter = true;
 
 });
-
-
-
-
+ 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
